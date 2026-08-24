49ea9c1f65d4a3b7af332887bf8373f92da25312
Oleksandr Zhylin <o.zhylin@proton.me>
2026-08-24 18:30:09 +0200

    Renamed several variables to better represent their function

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <yaml-cpp/yaml.h>
 #include <unordered_map>
 #include <cmath>
+#include <limits>
 
 
 struct transaction_req {
-    double cpu, ram, mem;
+    double cpu, ram, disc;
 };
 
 double readPositiveDouble(const std::string& prompt);
@@ -20,8 +21,9 @@ int main() {
     for (const auto& transaction : config["transactions"]) {
         transaction_types[transaction["type"].as<std::string>()] = {transaction["cpu"].as<double>(),
                                                                         transaction["ram"].as<double>(),
-                                                                        transaction["mem"].as<double>()};
+                                                                        transaction["disc"].as<double>()};
     }
+    auto safety_margin = config["safety_margin"].as<double>();
     double tx_number, time_limit;
     std::string tx_type;
     std::cout << "This is a hardware calculator." << std::endl;
@@ -34,10 +36,10 @@ int main() {
     }
     time_limit = readPositiveDouble("What is the time limit to finish all transactions in minutes?");
     double cpu_time = tx_number * transaction_types[tx_type].cpu;
-    double cores_num = std::ceil((( cpu_time / 1000 )/ (time_limit*60))*1.2);
-    double ram = cores_num * transaction_types[tx_type].ram;
-    double memory = tx_number * transaction_types[tx_type].mem;
-    std::cout << "You will need " << cores_num << " cores, " << ram /1000 << " MB of RAM and " << memory / 1000 <<" MB of memory"<<   std::endl;
+    double cores_num = std::ceil((( cpu_time / 1000 )/ (time_limit*60))*safety_margin);
+    double ram = cores_num * transaction_types[tx_type].ram * safety_margin;
+    double disc_space = tx_number * transaction_types[tx_type].disc * safety_margin;
+    std::cout << "You will need " << cores_num << " cores, " << ram /1000 << " MB of RAM and " << disc_space / 1000 <<" MB of disk space"<<   std::endl;
 }
 
 double readPositiveDouble(const std::string& prompt) {
